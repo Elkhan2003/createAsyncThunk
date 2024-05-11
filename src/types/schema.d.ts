cb492c802f95a4bb320024af77175ff0e16e255e
Elcho911 <boss.armsport@gmail.com>
2024-05-11 21:22:35 +0600

    .

---
+++ src/types/schema.d.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface Todo {
+	_id?: number;
+	title: string;
+	img: string;
+}
+
+namespace TODO {
+	type getReq = void;
+	type postReq = {
+		title: string;
+		img: string;
+	};
+	type EditReq = {
+		_id: number;
+		newData: NewDataType;
+	};
+	type deleteReq = {
+		_id: number;
+	};
+}
